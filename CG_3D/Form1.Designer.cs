b5165804436f34ed013a4dd0efa051c2dd5e50e9
joeljnr <joel_asj@outlook.com>
2017-10-20 16:53:46 -0200

    desenhando objetos 3D

--- CG_3D/Form1.Designer.cs
+++ CG_3D/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btCarrega = new System.Windows.Forms.Button();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btLateral = new System.Windows.Forms.Button();
+            this.btFrontal = new System.Windows.Forms.Button();
+            this.btSuperior = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,15 +60,60 @@
             this.btCarrega.UseVisualStyleBackColor = true;
             this.btCarrega.Click += new System.EventHandler(this.btCarrega_Click);
             // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(991, 654);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(146, 23);
+            this.btLimpar.TabIndex = 3;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btLateral
+            // 
+            this.btLateral.Location = new System.Drawing.Point(1009, 67);
+            this.btLateral.Name = "btLateral";
+            this.btLateral.Size = new System.Drawing.Size(116, 23);
+            this.btLateral.TabIndex = 4;
+            this.btLateral.Text = "Projeção Lateral";
+            this.btLateral.UseVisualStyleBackColor = true;
+            this.btLateral.Click += new System.EventHandler(this.btLateral_Click);
+            // 
+            // btFrontal
+            // 
+            this.btFrontal.Location = new System.Drawing.Point(1009, 96);
+            this.btFrontal.Name = "btFrontal";
+            this.btFrontal.Size = new System.Drawing.Size(116, 23);
+            this.btFrontal.TabIndex = 5;
+            this.btFrontal.Text = "Projeção Frontal";
+            this.btFrontal.UseVisualStyleBackColor = true;
+            this.btFrontal.Click += new System.EventHandler(this.btFrontal_Click);
+            // 
+            // btSuperior
+            // 
+            this.btSuperior.Location = new System.Drawing.Point(1009, 125);
+            this.btSuperior.Name = "btSuperior";
+            this.btSuperior.Size = new System.Drawing.Size(116, 23);
+            this.btSuperior.TabIndex = 6;
+            this.btSuperior.Text = "Projeção Superior";
+            this.btSuperior.UseVisualStyleBackColor = true;
+            this.btSuperior.Click += new System.EventHandler(this.btSuperior_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1149, 689);
+            this.Controls.Add(this.btSuperior);
+            this.Controls.Add(this.btFrontal);
+            this.Controls.Add(this.btLateral);
+            this.Controls.Add(this.btLimpar);
             this.Controls.Add(this.btCarrega);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -75,6 +124,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btCarrega;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btLateral;
+        private System.Windows.Forms.Button btFrontal;
+        private System.Windows.Forms.Button btSuperior;
     }
 }
 
--- CG_3D/Form1.cs
+++ CG_3D/Form1.cs
@@ -13,14 +13,116 @@ namespace CG_3D
     public partial class Form1 : Form
     {
         List<Objeto> objs = new List<Objeto>();
+        Bitmap bmp;
+        int largura;
+        int altura;
+        Objeto obj;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            altura = pictureBox1.Height / 2;
+            largura = pictureBox1.Width / 2;
+            limpar();
+        }
+
+
+        private void bresenham(int x1, int y1, int x2, int y2, Color cor)
+        {
+            int declive = 1;
+            int dx, dy, incE, incNE, d, x, y;
+            dx = x2 - x1;
+            dy = y2 - y1;
+
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                if (x2 < x1) // x2 < x1
+                    bresenham(x2, y2, x1, y1, cor);
+                else
+                {
+                    if (y2 < y1)
+                    {
+                        dy = -dy;
+                        declive = -1;
+                    }
+
+                    // Constante de Bresenham 
+                    incE = 2 * dy;
+                    incNE = 2 * dy - 2 * dx;
+                    d = incE - dx;
+                    y = y1;
+
+                    for (x = x1; x <= x2; x++)
+                    {
+
+                        if (y2 < y1)
+                        {
+                            if (x < pictureBox1.Width && y < pictureBox1.Height)
+                                bmp.SetPixel(x + largura, y + altura, cor);
+                        }
+                        else
+                            bmp.SetPixel(x + largura, y + altura, cor);
+                        if (d <= 0)
+                        {
+                            d += incE;
+                        }
+                        else
+                        {
+                            d += incNE;
+                            y += declive;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                if (y2 < y1) // x2 < x1
+                    bresenham(x2, y2, x1, y1, cor);
+                else
+                {
+                    if (x2 < x1)
+                    {
+                        dx = -dx;
+                        declive = -1;
+                    }
+
+                    // Constante de Bresenham 
+                    incE = 2 * dx;
+                    incNE = 2 * dx - 2 * dy;
+                    d = incE - dy;
+                    x = x1;
+
+                    for (y = y1; y <= y2; y++)
+                    {
+                        bmp.SetPixel(x + largura, y + altura, cor);
+                        if (d <= 0)
+                        {
+                            d += incE;
+                        }
+                        else
+                        {
+                            d += incNE;
+                            x += declive;
+                        }
+                    }
+                }
+
+            }
+
+        }
+
         private void btCarrega_Click(object sender, EventArgs e)
         {
-            Objeto obj = new Objeto();
+            limpar();
+
+            obj = new Objeto();
+
+            int i, j;
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -34,7 +136,7 @@ namespace CG_3D
                 sr.Close();
 
                 string[] aux = new string[4];
-                for (int i = 0; i < arq.Count; i++)
+                for (i = 0; i < arq.Count; i++)
                 {
                     aux = arq[i].Split();
                     if(aux[0].Equals("v"))
@@ -44,17 +146,122 @@ namespace CG_3D
                     else if (aux[0].Equals("f"))
                     {
                         Face f = new Face();
-                        //charAt
-                        f.Vertices.Add(obj.Originais[int.Parse(aux[1][0].ToString())]);
-                        f.Vertices.Add(obj.Originais[int.Parse(aux[2][0].ToString())]);
-                        f.Vertices.Add(obj.Originais[int.Parse(aux[3][0].ToString())]);
+                        string pto = "";
+                        int k;
+                        for (k = 0; k < aux[1].Length && aux[1][k] != '/'; k++) {
+                            pto += aux[1][k];
+                        }
+                        
+                        f.Vertices.Add(obj.Originais[int.Parse(pto) - 1]);
+                        pto = "";
+                        for (k = 0; k < aux[2].Length && aux[2][k] != '/'; k++)
+                        {
+                            pto += aux[2][k];
+                        }
+
+                        f.Vertices.Add(obj.Originais[int.Parse(pto) - 1]);
+                        pto = "";
+                        for (k = 0; k < aux[3].Length && aux[3][k] != '/'; k++)
+                        {
+                            pto += aux[3][k];
+                        }
+
+                        f.Vertices.Add(obj.Originais[int.Parse(pto) - 1]);
                         obj.Faces.Add(f);
                     }
                 }
-            }
+                
 
-            for (int i = 0; i < obj.Originais.Count; i++)
-                Console.Write(obj.Originais.ToString());
+
+                for(i = 0; i < obj.Faces.Count; i++)
+                {
+                    for(j = 0; j < obj.Faces[i].Vertices.Count - 1; j++)
+                    {
+                        bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[j+1].X, (int)obj.Faces[i].Vertices[j+1].Y, Color.FromArgb(0,0,0));
+                    }
+
+                    bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[0].X, (int)obj.Faces[i].Vertices[0].Y, Color.FromArgb(0, 0, 0));
+                }
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void limpar() {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            for (int x = 0; x < pictureBox1.Width; x++)
+            {
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    bmp.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            limpar();
+        }
+
+        private void btLateral_Click(object sender, EventArgs e)
+        {
+            limpar();
+            int i, j;
+            if (obj != null) {
+                for (i = 0; i < obj.Faces.Count; i++)
+                {
+                    for (j = 0; j < obj.Faces[i].Vertices.Count - 1; j++)
+                    {
+                        bresenham((int)obj.Faces[i].Vertices[j].Z, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[j + 1].Z, (int)obj.Faces[i].Vertices[j + 1].Y, Color.FromArgb(0, 0, 0));
+                    }
+
+                    bresenham((int)obj.Faces[i].Vertices[j].Z, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[0].Z, (int)obj.Faces[i].Vertices[0].Y, Color.FromArgb(0, 0, 0));
+                }
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void btFrontal_Click(object sender, EventArgs e)
+        {
+            limpar();
+            int i, j;
+            if (obj != null)
+            {
+                for (i = 0; i < obj.Faces.Count; i++)
+                {
+                    for (j = 0; j < obj.Faces[i].Vertices.Count - 1; j++)
+                    {
+                        bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[j + 1].X, (int)obj.Faces[i].Vertices[j + 1].Y, Color.FromArgb(0, 0, 0));
+                    }
+
+                    bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Y, (int)obj.Faces[i].Vertices[0].X, (int)obj.Faces[i].Vertices[0].Y, Color.FromArgb(0, 0, 0));
+                }
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void btSuperior_Click(object sender, EventArgs e)
+        {
+            limpar();
+            int i, j;
+            if (obj != null)
+            {
+                for (i = 0; i < obj.Faces.Count; i++)
+                {
+                    for (j = 0; j < obj.Faces[i].Vertices.Count - 1; j++)
+                    {
+                        bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Z, (int)obj.Faces[i].Vertices[j + 1].X, (int)obj.Faces[i].Vertices[j + 1].Z, Color.FromArgb(0, 0, 0));
+                    }
+
+                    bresenham((int)obj.Faces[i].Vertices[j].X, (int)obj.Faces[i].Vertices[j].Z, (int)obj.Faces[i].Vertices[0].X, (int)obj.Faces[i].Vertices[0].Z, Color.FromArgb(0, 0, 0));
+                }
+
+                pictureBox1.Image = bmp;
+            }
         }
     }
 }
